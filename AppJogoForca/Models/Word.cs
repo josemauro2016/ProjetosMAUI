e3c73060c295c23a893efea819396519315bbfa7
Jose Mauro Mendes Junior <josemauromendesjunior@yahoo.com.br>
2026-02-17 17:44:13 -0300

    Aula 79

---
+++ AppJogoForca/Models/Word.cs
@@ -0,0 +1,12 @@
+﻿namespace AppJogoForca.Models;
+
+public class Word
+{
+    public Word(String Tips, String Text)
+    {
+        this.Tips = Tips;
+        this.Text = Text;
+    }
+    public String Tips { get; set; } = String.Empty;
+    public String Text { get; set; } = String.Empty;
+}
---
+++ AppJogoForca/Repositories/WordRepositories.cs
@@ -0,0 +1,26 @@
+﻿using AppJogoForca.Models;
+
+namespace AppJogoForca.Repositories;
+
+public class WordRepositories
+{
+    private List<Word> _words;
+
+    public WordRepositories()
+    {
+        _words = new List<Word>();
+        _words.Add(new Word("Nome", "Maria"));
+        _words.Add(new Word("Vegetal", "Cenoura"));
+        _words.Add(new Word("Fruta", "Abacate"));
+        _words.Add(new Word("Tempero", "Alho"));
+        _words.Add(new Word("Carro", "Fusca"));
+        _words.Add(new Word("Esporte", "Ciclismo"));
+    }
+
+    public Word GetRandomWord()
+    {
+        Random rand = new Random();
+        var number = rand.Next(0, _words.Count);
+        return _words[number];
+    }
+}
