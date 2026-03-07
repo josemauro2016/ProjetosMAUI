23374d5a097678cd358e18f62b48afe1a4468b10
Jose Mauro Mendes Junior <josemauromendesjunior@yahoo.com.br>
2026-03-06 23:30:26 -0300

    Aula 84

--- AppJogoForca/MainPage.xaml.cs
+++ AppJogoForca/MainPage.xaml.cs
@@ -18,37 +18,72 @@ namespace AppJogoForca
         
         private async void OnButtonClick(object sender, EventArgs e)
         {
-            ((Button)sender).IsEnabled = false;
-            String letter = ((Button)sender).Text;
+            Button button = (Button)sender;
+            button.IsEnabled = false;
+            String letter = button.Text;
 
             var positions = _word.Text.GetPositions(letter);
 
             if (positions.Count == 0)
             {
-                _errors++;
-
-                ImgMain.Source = ImageSource.FromFile($"forca{_errors + 1}.png");
-
-                if(_errors == 6)
-                {
-                    await DisplayAlert("Perdeu!", "Já tá enforcado", "Novo Jogo");
-                    ResetScreen();
-                }
+                ErrorHandler(button);
+                await IsGameOver();
                 return;
             }
 
-            foreach ( var position in positions )
+            ReplaceLetter(letter, positions);
+            button.Style = App.Current.Resources.MergedDictionaries.ElementAt(1)["Success"] as Style;
+
+            await HasWinner();
+        }
+        
+        private void OnButtonClickedResetGame(object sender, EventArgs e)
+        {
+            ResetScreen();
+        }
+
+        #region Handler Success
+        private void ReplaceLetter(string letter, List<int> positions)
+        {
+            foreach (var position in positions)
             {
                 LblText.Text = LblText.Text.Remove(position, 1).Insert(position, letter);
             }
         }
 
+        private async Task HasWinner()
+        {
+            if (!LblText.Text.Contains("_"))
+            {
+                await DisplayAlert("Parabéns!", "Você acertou!", "Novo Jogo");
+                ResetScreen();
+            }
+        }
+        #endregion
+        #region Handler Error
+        private void ErrorHandler(Button button)
+        {
+            _errors++;
+            ImgMain.Source = ImageSource.FromFile($"forca{_errors + 1}.png");
+            button.Style = App.Current.Resources.MergedDictionaries.ElementAt(1)["Fail"] as Style;
+        }
+
+        private async Task IsGameOver()
+        {
+            if (_errors == 6)
+            {
+                await DisplayAlert("Perdeu!", "Já tá enforcado", "Novo Jogo");
+                ResetScreen();
+            }
+        }
+        #endregion
+        #region Reset Screen - Back Screen to Initial State
         private void ResetScreen()
         {
             ResetVirtualKeyboard();
             ResetErrors();
             GenerateNewWord();
-            
+
         }
 
         private void GenerateNewWord()
@@ -70,17 +105,20 @@ namespace AppJogoForca
             ResetVirtualLines((HorizontalStackLayout)KeyBoardContainer.Children[0]);
             ResetVirtualLines((HorizontalStackLayout)KeyBoardContainer.Children[1]);
             ResetVirtualLines((HorizontalStackLayout)KeyBoardContainer.Children[2]);
-            
-           
+
+
         }
 
         private void ResetVirtualLines(HorizontalStackLayout horizontal)
         {
-            foreach(Button button in horizontal.Children)
+            foreach (Button button in horizontal.Children)
             {
                 button.IsEnabled = true;
+                button.Style = null;
             }
         }
+        #endregion
+        
     }
 
 }
