9fc9bdbdecc320e067c435680871bfc67a970c7f
Jose Mauro Mendes Junior <josemauromendesjunior@yahoo.com.br>
2026-04-01 23:24:55 -0300

    Aula 116

--- AppMauiGallery/Views/Lists/ListViewPage.xaml.cs
+++ AppMauiGallery/Views/Lists/ListViewPage.xaml.cs
@@ -1,4 +1,5 @@
 using AppMauiGallery.Views.Lists.Models;
+using System.Threading.Tasks;
 
 namespace AppMauiGallery.Views.Lists;
 
@@ -13,6 +14,24 @@ public partial class ListViewPage : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        ListViewControl.ItemsSource = MovieList.GetList();
+        ListViewControl.ItemsSource = MovieList.GetList().Take(2);
     }
+
+    private void ListViewControl_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        var movie = (Movie)e.SelectedItem;
+
+        App.Current.MainPage.DisplayAlert("Filme selecionado!", $"O filme selecionado é: {movie.Name}", "OK");
+    }
+
+    private async void ListViewControl_Refreshing(object sender, EventArgs e)
+    {
+        ListViewControl.IsRefreshing = true;
+        await Task.Delay(3000);
+        ListViewControl.ItemsSource = MovieList.GetList();
+
+        ListViewControl.IsRefreshing = false;
+    }
+
+   
 }
