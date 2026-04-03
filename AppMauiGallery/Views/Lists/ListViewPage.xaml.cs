5634d67ea78de48d13580c4a9df10f17f6c28b26
Jose Mauro Mendes Junior <josemauromendesjunior@yahoo.com.br>
2026-04-02 23:19:46 -0300

    Aula 117

--- AppMauiGallery/Views/Lists/ListViewPage.xaml.cs
+++ AppMauiGallery/Views/Lists/ListViewPage.xaml.cs
@@ -14,7 +14,7 @@ public partial class ListViewPage : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        ListViewControl.ItemsSource = MovieList.GetList().Take(2);
+        ListViewControl.ItemsSource = MovieList.GetGroupList().Take(2);
     }
 
     private void ListViewControl_ItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -28,7 +28,7 @@ public partial class ListViewPage : ContentPage
     {
         ListViewControl.IsRefreshing = true;
         await Task.Delay(3000);
-        ListViewControl.ItemsSource = MovieList.GetList();
+        ListViewControl.ItemsSource = MovieList.GetGroupList();
 
         ListViewControl.IsRefreshing = false;
     }
